5e88b79638b28324934ae1102453ec571041e2cd
jong-geungwon <ngc@imform.co.kr>
2021-05-08 22:21:03 +0900

    spring security 적용 및 password encoder 추가

--- src/main/java/com/example/userservice/service/MemberServiceImpl.java
+++ src/main/java/com/example/userservice/service/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.userservice.entity.Member;
 import com.example.userservice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +14,14 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public ResponseMemberDTO createMember(CreateMemberDTO createMemberDTO) {
         var member = modelMapper.map(createMemberDTO, Member.class);
+
+        member.setPassword(bCryptPasswordEncoder.encode(member.getPassword()));
+
         var createdMember = memberRepository.save(member);
 
         return modelMapper.map(createdMember, ResponseMemberDTO.class);
