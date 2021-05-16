88220e3591857c642e4048c3e6a4e0137df9f4e4
jong-geungwon <ngc@imform.co.kr>
2021-05-16 23:41:25 +0900

    feign client 를 이용한 order-service 호출

---
+++ src/main/java/com/example/userservice/client/OrderServiceClient.java
@@ -0,0 +1,16 @@
+package com.example.userservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+
+@FeignClient(name = "order-service")
+public interface OrderServiceClient {
+
+    @GetMapping("/order-service/{userId}/orders")
+    ResponseEntity<Map<String, Object>> getOrders(@PathVariable("userId")  String userId);
+
+}
